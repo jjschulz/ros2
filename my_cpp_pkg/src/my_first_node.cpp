64ba7dd1a43b74c3e47a85bce60f7b037bb3bec7
Johnna <johnnajanelle01@gmail.com>
2026-09-02 13:48:49 +0000

    add to cmake/cpp

--- my_cpp_pkg/src/my_first_node.cpp
+++ my_cpp_pkg/src/my_first_node.cpp
@@ -1,19 +1,35 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode : public rclcpp::Node{
+    public:
+        MyNode() : Node("cpp_test"), counter_(0){
+            RCLCPP_INFO(this->get_logger(), "hello World");
+            // calls logger every 1 second
+            timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&MyNode::timerCallback, this));
+        }
+    private:
+        void timerCallback(){
+            RCLCPP_INFO(this->get_logger(), "hello %d", counter_);
+            counter_++;
+        }
+        rclcpp::TimerBase::SharedPtr timer_;
+        int counter_;
+};
+
 int main(int argc, char **argv){
     //first thing always
     rclcpp::init(argc, argv);
 
     // auto type
     // shared pointer to Node object
-    auto node = std::make_shared<rclcpp::Node>();
+    auto node = std::make_shared<MyNode>();
     // dot interacts with shared pointer, 
     // arrow interacts with reference to shared pointer
-    RCLCPP_INFO(node->get_logger(), "Hello World");
+    // RCLCPP_INFO(node->get_logger(), "Hello World");
+
+    rclcpp::spin(node);
 
     //last thing always
-    rclcpp:shutdown();
+    rclcpp::shutdown();
     return 0;
 }
-
-// how to build code
