0e33ccbe4ffe42e0b597d3c2a931446c4bc437eb
Johnna <johnnajanelle01@gmail.com>
2026-09-16 23:50:46 +0000

    activity 3

--- my_py_pkg/my_py_pkg/number_counter.py
+++ my_py_pkg/my_py_pkg/number_counter.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import String
+from example_interfaces.srv import SetBool
 
 
 
@@ -12,6 +13,21 @@ class NumberCounterNode(Node):
 
         self.publisher_ = self.create_publisher(String, "number_count", 10)
         # self.timer_ = self.create_timer(0.5, self.publish_news)
+
+        self.server_ = self.create_service(SetBool, "reset_counter", 
+                                                   self.callback_func_reset)
+        
+    def callback_func_reset(self, request: SetBool.Request, response: SetBool.Response):
+        reset_if_true = request.data
+        if(reset_if_true):
+                self.counter_ = 0
+                response.success = True
+                self.get_logger().info('resetting counter')
+        else:
+              response.success = False
+        
+        # self.get_logger().info(str(request.a)+ "+" + str(request.b) + "=" + str(response.sum))
+        return response
 
     def callback_function(self, msg: String):
             # self.get_logger().info(msg.data)
