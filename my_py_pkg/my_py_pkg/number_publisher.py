ec4172244c8db872f043c2c31a1607e59133a864
Johnna <johnnajanelle01@gmail.com>
2026-09-23 20:57:48 +0000

    section 8 plus activity

--- my_py_pkg/my_py_pkg/number_publisher.py
+++ my_py_pkg/my_py_pkg/number_publisher.py
@@ -8,12 +8,17 @@ from example_interfaces.msg import String
 class NumberPublisherNode(Node): 
     def __init__(self):
         super().__init__("number_publisher") 
+        # declare the name and the default value
+        self.declare_parameter("number", "2")
+        self.declare_parameter("timer_period", 1.0)
+        self.number_ = self.get_parameter("number").value
+        self.timer_period_ = self.get_parameter("timer_period").value
         self.publisher_ = self.create_publisher(String, "number", 10)
-        self.timer_ = self.create_timer(0.5, self.publish_news)
+        self.timer_ = self.create_timer(self.timer_period_, self.publish_news)
 
     def publish_news(self):
             msg = String()
-            msg.data = "2"
+            msg.data = self.number_
             self.publisher_.publish(msg)
 
 def main(args=None):
